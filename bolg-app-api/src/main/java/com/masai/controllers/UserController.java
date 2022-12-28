98d4002323a69467e77e800ffb0c2df1ab3e5d5f
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-28 16:26:47 +0530

    user crud operation done

--- bolg-app-api/src/main/java/com/masai/controllers/UserController.java
+++ bolg-app-api/src/main/java/com/masai/controllers/UserController.java
@@ -1,11 +1,8 @@
 package com.masai.controllers;
 
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.masai.entities.User;
 import com.masai.payloads.ApiResponse;
 import com.masai.payloads.UserDto;
 import com.masai.services.UserService;

9a34d30fab6b726b36ef6bd11e0f8bd2abb92488
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-28 09:21:00 +0530

    database configuration done

---
+++ bolg-app-api/src/main/java/com/masai/entities/User.java
@@ -0,0 +1,34 @@
+package com.masai.entities;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity 
+@Table(name="users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column(name="user_name", nullable=false, length=100)
+	private String name;
+	private String email;
+	private String password;
+	private String about;
+	
+
+}

af595a134a4bdeb3e17c8a4ad453b6cd03ac425e
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-29 07:55:59 +0530

    comment for cheking the implementing class

--- bolg-app-api/src/test/java/com/masai/BolgAppApiApplicationTests.java
+++ bolg-app-api/src/test/java/com/masai/BolgAppApiApplicationTests.java
@@ -1,9 +1,10 @@
 package com.masai;
 
 import org.junit.jupiter.api.Test;
-
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//import com.masai.services.UserService;
 
 @SpringBootTest
 class BolgAppApiApplicationTests {
@@ -13,13 +14,15 @@ class BolgAppApiApplicationTests {
 	}
 
 //	@Autowired
-//	private UserRepo re;
-//
+//	private UserService re;
+//	
 //	@Test
 //	public void repoTest() {
 //
 //		String classN = this.re.getClass().getName();
 //		System.out.println(classN);
 //	}
+
+//	this for which to know which class is implimenting the interface
 
 }

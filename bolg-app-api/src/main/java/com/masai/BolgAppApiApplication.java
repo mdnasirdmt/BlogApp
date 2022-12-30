5cad3672b39bc5be23f8a72743a07db858931899
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-30 19:10:47 +0530

    post entity crud operation

--- bolg-app-api/src/main/java/com/masai/BolgAppApiApplication.java
+++ bolg-app-api/src/main/java/com/masai/BolgAppApiApplication.java
@@ -3,9 +3,10 @@ package com.masai;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class BolgAppApiApplication {
 
 	public static void main(String[] args) {
--- bolg-app-api/src/main/java/com/masai/exceptions/GlobalExceptionHandler.java
+++ bolg-app-api/src/main/java/com/masai/exceptions/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-//import com.masai.payloads.ApiResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {

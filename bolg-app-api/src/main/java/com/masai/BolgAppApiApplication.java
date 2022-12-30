8ddf208a89f543835f2b6a9ff8ea957c2ad403be
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-30 22:08:46 +0530

    clear warnig

--- bolg-app-api/src/main/java/com/masai/BolgAppApiApplication.java
+++ bolg-app-api/src/main/java/com/masai/BolgAppApiApplication.java
@@ -3,7 +3,7 @@ package com.masai;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication()
@@ -20,3 +20,4 @@ public class BolgAppApiApplication {
 	}
 
 }
+
